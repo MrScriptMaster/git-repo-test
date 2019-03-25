ab7df448418502f38887cc23c989d79b5edb204b
gokhmak <gokhmak@rocketsoftware.com>
2019-03-25 15:15:21 +0500

    Added idioms

---
+++ idioms/manipulators.cpp
@@ -0,0 +1,293 @@
+#include <cassert>
+#include <fstream>
+#include <list>
+#include <stack>
+#include <string>
+
+/*
+ * ATTENTION: Not debugged
+ */
+
+class BaseStream;
+
+namespace Manips {
+    /*
+     * If
+     */
+    class CIf {
+    public:
+        bool m_condition;
+        CIf(bool condition) : m_condition(condition) {}
+    }; // If
+    const CIf If(bool codition);
+    BaseStream& operator << (BaseStream&, const CIf&);
+    /*
+     * Else
+     */
+    class  CElse {};
+    extern CElse Else;
+    BaseStream& operator << (BaseStream&, const CElse&);
+    /*
+     * EndIf
+     */
+    class CEndIf {};
+    extern CEndIf EndIf;
+    BaseStream& operator << (BaseStream&, const CEndIf&);
+    /*
+     * CIntend
+     */
+    class CIndented;
+    BaseStream& operator << (BaseStream&, CIndented&&);
+    class CIndented {
+        std::string m_string;
+    public:
+        CIndented(const std::string& string)
+            : m_string(string)
+        {}
+        friend BaseStream& operator << (BaseStream&, CIndented&&);
+    };
+    CIndented Indent(const std::string&);
+} // namespace Manips
+
+class BaseStream {
+public:
+    BaseStream();
+    virtual ~BaseStream() = default;
+           BaseStream& operator << (char);
+           BaseStream& operator << (const char*);
+    inline BaseStream& operator << (const std::string&);
+           BaseStream& operator << (int);
+
+protected:
+    virtual void write(char) =0;
+    uint m_currentColumn;
+    inline bool is_write_suppressed() const
+    {
+        return !m_allowed.top();
+    }
+    inline bool invariant() const
+    {
+        return !m_allowed.empty();
+    }
+
+    std::stack<bool> m_allowed;
+    friend BaseStream& Manips::operator<< (BaseStream&, const Manips::CIf&);
+    friend BaseStream& Manips::operator<< (BaseStream&, const Manips::CElse&);
+    friend BaseStream& Manips::operator<< (BaseStream&, const Manips::CEndIf&);
+    friend BaseStream& Manips::operator<< (BaseStream&, Manips::CIndented&&);
+};
+
+class CustomStream : public BaseStream {
+    std::string   m_streamName;
+    std::ofstream m_ostr;
+
+public:
+    CustomStream(const char* streamName);
+    ~CustomStream();
+
+protected:
+    virtual void write(char) override;
+}; // CustomStream
+
+// --- cut off ---------------------------------------------------- cut off ---
+
+#include <cstring>
+
+using namespace std;
+
+//
+// BaseStream
+//
+
+namespace math {
+    void reverse(char s[])
+    {
+        int i, j;
+        char c;
+    
+        for (i = 0, j = strlen(s)-1; i<j; i++, j--) {
+            c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+        }
+    }
+    void itoa(int n, char s[])
+    {
+        int i, sign;
+    
+        if ((sign = n) < 0)
+            n = -n;
+        i = 0;
+        do {
+            s[i++] = n % 10 + '0';
+        } while ((n /= 10) > 0);
+        if (sign < 0)
+            s[i++] = '-';
+        s[i] = '\0';
+        reverse(s);
+    }
+}
+
+BaseStream::BaseStream()
+    : m_currentColumn(0)
+{
+    m_allowed.push(true);
+    assert(invariant());
+}
+
+BaseStream& BaseStream::operator<< (char c)
+{
+    assert(invariant());
+    if (is_write_suppressed()) return *this;
+    write(c);
+    m_currentColumn++;
+    if ('\n' == c)  m_currentColumn = 0;
+    assert(invariant());
+    return *this;
+}
+
+BaseStream& BaseStream::operator<< (const char* cMass)
+{
+    if (is_write_suppressed()) return *this;
+    for (size_t i = 0; cMass[i] != '\0'; i++) {
+        *this << cMass[i];
+    }
+    return *this;
+}
+
+BaseStream& BaseStream::operator<< (const string& input)
+{
+    return ((*this) << input.c_str());
+}
+
+BaseStream& BaseStream::operator<< (int value)
+{
+    char buffer[256];
+    math::itoa(value, buffer);
+    return ((*this) << string(buffer) );
+}
+
+//
+// CustomStream
+//
+
+CustomStream::CustomStream(const char* streamName)
+    : BaseStream()
+    ,m_streamName(streamName)
+{
+    m_ostr.open(m_streamName, std::ios::out | std::ios::trunc);
+}
+
+CustomStream::~CustomStream()
+{
+    if (m_ostr.is_open())
+        m_ostr.close();
+}
+
+void CustomStream::write(char c)
+{
+    m_ostr << c;
+}
+
+//
+// If
+//
+
+const Manips::CIf Manips::If(bool condition)
+{
+    return Manips::CIf(condition);
+}
+
+BaseStream& Manips::operator<< (BaseStream& stream, const Manips::CIf& If)
+{
+    assert(stream.invariant());
+    stream.m_allowed.push(If.m_condition && stream.m_allowed.top());
+    assert(stream.invariant());
+    return stream;
+}
+
+//
+// Else
+//
+
+Manips::CElse Manips::Else;
+BaseStream& Manips::operator<< (BaseStream& stream, const Manips::CElse& Else)
+{
+    assert(stream.invariant());
+    bool lastAllowed = stream.m_allowed.top();
+    stream.m_allowed.pop();
+    stream.m_allowed.push(!lastAllowed && stream.m_allowed.top());
+    assert(stream.invariant());
+    return stream;
+}
+
+//
+// EndIf
+//
+
+Manips::CEndIf Manips::EndIf;
+
+BaseStream& Manips::operator << (BaseStream& stream, const Manips::CEndIf& EndIf)
+{
+    assert(stream.invariant());
+    stream.m_allowed.pop();
+    assert(stream.invariant());
+    return stream;
+}
+
+//
+// CIndented
+//
+
+BaseStream& Manips::operator<< (BaseStream& stream, Manips::CIndented&& intendedString)
+{
+    assert(stream.invariant());
+    uint curColumn = stream.m_currentColumn;
+    for (size_t i = 0; i < intendedString.m_string.size(); i++)
+    {
+        char c = intendedString.m_string[i];
+        stream << c;
+        if (c == '\n') {
+            for (size_t j = 0; j < curColumn; j++) {
+                stream << ' ';
+            }
+        }
+    }
+    assert(stream.invariant());
+    return stream;
+}
+
+Manips::CIndented Manips::Indent(const std::string& inputString)
+{
+    return CIndented(inputString);
+}
+
+// --- cut off ---------------------------------------------------- cut off ---
+
+#include <iostream>
+
+using namespace Manips;
+
+int main(int argc, char* argv[])
+{
+    cout << "--- Start write to file ---" << endl;
+    CustomStream TestFile("test.txt");
+    TestFile
+        << "Hello, World!" << "\n"
+        << If (true)
+        <<   "Print this 1" << "\n"
+        << EndIf
+        << If (false)
+        <<   "Do not print this" << "\n"
+        << EndIf
+        << If (false)
+        <<    "Do not print this" << "\n"
+        << Else
+        <<    "Print this 2" << "\n"
+        << EndIf
+        << "   " << Indent("Alpha\nBeta\nGamma") << "\n"
+        << "Number = " << 15 << "\n";
+
+    cout << "--- End write to file ---" << endl;
+    return 0;
+}
---
+++ idioms/useful_macro_defs.cpp
@@ -0,0 +1,126 @@
+/**
+ * @file useful_macro_defs.cpp
+ * @author Grigory Okhmak (ohmak88@yandex.ru)
+ * @details 
+ * В этом файле приведены некоторые полезные макросы, позволяющие сэкономить
+ * немного времени на написание прототипов.
+ * @version 0.1
+ * @date 2019-02-05
+ * 
+ * @copyright GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+ * 
+ */
+
+/**
+ * @brief Данный макрос позволяет конструировать строки мз литералов. Им
+ * рекомендуется пользоваться, когда некоторые макросы разрешаются строками,
+ * приходящими во время вызова компилятора.
+ * 
+ * @example 
+ * @code 
+ * "Hello, "__macro_make_string(World!)
+ * @endcode
+ */
+#define __macro_make_string__(arg) #arg
+#define __macro_make_string(arg) __macro_make_string__(arg)
+
+/**
+ * @brief Данный макрос позволяет конкатенировать два литерала.
+ * 
+ * @example
+ * @code
+ * __macro_join(Hello, World!)
+ * @endcode
+ */
+#define __macro_join__(arg1,arg2) arg1##arg2
+#define __macro_join(arg1,arg2) __macro_join__(arg1,arg2)
+
+/**
+ * @brief Данный макрос позволяет быстро создавать атрибуты вместе с
+ * геттерами и сеттерами для классов и структур.
+ * 
+ * @example
+ * @code
+ * class CTest {
+ *     __macro_make_class_attr(int, Number)
+ *     __macro_make_class_attr(std::string, String)
+ * };
+ * @endcode
+ */
+#define __macro_make_class_attr(type,name) \
+private: type m_##name;\
+public:\
+    const type& get##name() const { return m_##name; }\
+    void set##name(const type& value) { m_##name = value; }
+
+#ifndef __STDC_VERSION__
+#define __STDC_VERSION__ -1//__macro_make_string("__STDC_VERSION__ is not supported")
+#endif
+
+#ifndef __STDC_NO_COMPLEX__
+#define __STDC_NO_COMPLEX__ -1//__macro_make_string("__STDC_NO_COMPLEX__ is not supported")
+#endif
+
+#ifndef __STDC_NO_VLA__
+#define __STDC_NO_VLA__ -1//__macro_make_string("__STDC_NO_VLA__ is not supported")
+#endif
+
+#ifndef __VA_ARGS__
+#define __VA_ARGS__ -1//__macro_make_string("__VA_ARGS__ is not supported")
+#endif
+
+#define NDEBUG
+
+#include <cassert>
+#include <iostream>
+#include <string>
+
+class TryMacros {
+    __macro_make_class_attr(int, Number)
+    __macro_make_class_attr(std::string, String)
+public:
+    TryMacros(int number, const std::string& string)
+        : m_Number(number)
+        ,m_String(string)
+    {}
+};
+
+using namespace std;
+
+void function() {
+    cout << "Processed on " << __LINE__ << " line" << endl;
+    cout << "Processed in function named by " << __FUNCTION__ << endl;
+    cout << "COUNTER IS " << __COUNTER__ << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    assert("Macro test");
+    TryMacros  test(12,"hello");
+    cout << test.getNumber() << " " << test.getString() << endl;
+    int __macro_join(test,var) = 15;
+    cout << "Macro join = " << testvar << endl;
+    cout << "Hello, "__macro_make_string(World!) << endl;
+    // Стандартные макросы
+    cout << "Processed on " << __LINE__ << " line" << endl;
+    cout << "Source file name is " << __FILE__ << endl;
+    cout << "COUNTER IS " << __COUNTER__ << endl;
+    cout << "Preprocessor launched in: " __DATE__ << endl
+                                     <<  __TIME__ << endl
+                                     <<  __TIMESTAMP__ << endl;
+    
+    /* __STDC__ заменяется на 1, если компиляция происходит в соответствии со стандартом языка С
+     * 
+     */                                 
+    cout << "Compile with standard: " << __STDC__ << " or " << __STDC_VERSION__ << endl
+         << "Program has been controlling by: " << __STDC_HOSTED__ << endl;
+    cout << "System support:" << endl
+         << "-- operations with floating numbers = " << __STDC_IEC_559__ << endl
+         << "-- comlex numbers = " << __STDC_IEC_559_COMPLEX__ << " and " << __STDC_NO_COMPLEX__ << endl
+         << "-- masses with variable length = " << __STDC_NO_VLA__ << endl
+         << "-- macros' with variable args = " << __VA_ARGS__ << endl
+         ;
+    
+    function();
+    return 0;
+}
