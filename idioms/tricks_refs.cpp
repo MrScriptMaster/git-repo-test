1f4fa28c1a2acc8603e94eec628281b111fe16d7
Grigorii <ohmak88@ya.ru>
2019-10-21 10:11:47 +0300

    Update tricks_refs.cpp

--- idioms/tricks_refs.cpp
+++ idioms/tricks_refs.cpp
@@ -15,10 +15,16 @@ using inst = A& (&)();
 
 inst manager = A::instance;
 
+template<typename C>
+using tinst = C& (&)();
+
 int main(int argc, char* argv[])
 {
     std::cout << manage().hello() << std::endl;
     std::cout << manager().hello() << std::endl;
+
+    tinst<A> mng = A::instance;
+    std::cout << mng().hello() << std::endl;
 
     return 0;
 }
