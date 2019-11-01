b32fbf55da072322abd985122eb9fd36195ed817
Grigorii <ohmak88@ya.ru>
2019-11-01 13:50:16 +0300

    Update packing.cpp

--- c++11/packing.cpp
+++ c++11/packing.cpp
@@ -1,21 +1,88 @@
+#include <iostream>
 #include <utility>
 #include <string>
 #include <fstream>
-#include <iostream>
+#include <sstream>
 #include <algorithm>
+#include <functional>
+#include <vector>
 
-class CAudiotunePackage
+
+#define MASKED_SPACE ','
+
+class CPacker
 {
 public:
-    CAudiotunePackage(const std::string& sPath)
+    using string_processing = std::function<void(std::string&)>;
+    using iterator = std::vector<string_processing>::iterator;
+
+    //Helpers
+    static string_processing trimmer()
+    {
+        return [](std::string& s)
+            {
+                const char whitespace[] {" \t\n"};
+                const std::size_t first(s.find_first_not_of(whitespace));
+                if (std::string::npos == first) { return; }
+                const std::size_t last(s.find_last_not_of(whitespace));
+                s = std::move(s.substr(first, (last - first + 1)));
+            };
+    }
+    static string_processing extra_space_remover()
+    {
+        return [](std::string& s)
+            {
+                std::string sTmp;
+                std::unique_copy (s.begin(), s.end(),
+                                    std::back_insert_iterator<std::string>(sTmp),
+                                    [](char a,char b){ return isspace(a) && isspace(b);});
+                s = std::move(sTmp);
+            };
+    }
+    static string_processing append_new_line()
+    {
+        return [](std::string& s)
+            {
+                s += "\n";
+            };
+    }
+    static string_processing space_masking()
+    {
+        return [](std::string& s)
+        {
+            std::replace_if(s.begin(), s.end(),
+                [](char c)->bool { return (isspace(c)); }, MASKED_SPACE);
+        };
+    }
+    
+    CPacker(const std::string& sTarget, bool bIsFilePath = true)
         : m_packageSize(1499)
-        ,m_File(sPath)
-    {}
-    CAudiotunePackage(std::size_t packageSize, const std::string& sPath)
+        ,m_bFileStream(bIsFilePath)
+    {
+        if (isFileStream())
+        {
+            m_File.open(sTarget);
+        }
+        else
+        {
+            m_sStream = std::istringstream(sTarget);
+        }
+    }
+    CPacker(std::size_t packageSize, const std::string& sTarget, bool bIsFilePath = true)
         : m_packageSize(packageSize)
-        ,m_File(sPath)
-    {}
-    virtual ~CAudiotunePackage()
+        ,m_bFileStream(bIsFilePath)
+    {
+        m_packageSize = std::max((std::size_t)1, m_packageSize);
+        if (isFileStream())
+        {
+            m_File.open(sTarget);
+        }
+        else
+        {
+            m_sStream = std::move(std::istringstream(sTarget));
+        }
+    }
+    virtual ~CPacker()
     {
         if (m_File.is_open())
         {
@@ -29,52 +96,30 @@ public:
      */
     const char* getPackage()
     {
-        if (!m_File.is_open())
+        std::istream* pS = NULL;
+        if (isFileStream())
         {
-            return NULL;
+            if (!m_File.is_open())
+            {
+                return NULL;
+            }
+            pS = static_cast<std::istream*>(&m_File);
         }
+        else
+        {
+            pS = static_cast<std::istream*>(&m_sStream);
+        }
+
         m_sBuffer = m_sResidue;
         m_sResidue.clear();
-        char delimeter = '\n';
-        char maskingCharacter = ',';
 
-        while (std::getline(m_File, m_sResidue, delimeter))
-        {
-            // truncate very long strings
-            if (m_sResidue.size() > m_packageSize)
-                m_sResidue.resize(m_packageSize);
-            
-            // Triming
-            [](std::string& s)
+        while (std::getline(*pS, m_sResidue))
+        {           
+            for (iterator it = m_vProcess.begin(); it != m_vProcess.end(); ++it)
             {
-                const char whitespace[] {" \t\n"};
-                const std::size_t first(s.find_first_not_of(whitespace));
-                if (std::string::npos == first) { return; }
-                const std::size_t last(s.find_last_not_of(whitespace));
-                s = std::move(s.substr(first, (last - first + 1)));
-            }(m_sResidue);
-            
-            // Remove extra spaces
-            [](std::string& s)
-            {
-                std::string sTmp;
-                std::unique_copy (s.begin(), s.end(),
-                                    std::back_insert_iterator<std::string>(sTmp),
-                                    [](char a,char b){ return isspace(a) && isspace(b);});
-                s = std::move(sTmp);
-            }(m_sResidue);
+                (*it)(m_sResidue);
+            }
 
-            // Replace spaces to masking character 
-            // (note: the client truncates the message by the first space,
-            //  so we mask them like that)
-            [&maskingCharacter](std::string& s)
-            {
-                std::replace_if(s.begin(), s.end(),
-                                    [](char c)->bool { return (isspace(c)); }, maskingCharacter);
-            }(m_sResidue);
-
-            // Append delimeter back
-            m_sResidue += delimeter;
             if ((m_sBuffer.size() + m_sResidue.size()) < m_packageSize)
             {
                 m_sBuffer.append(m_sResidue);
@@ -92,27 +137,52 @@ public:
         }
         return m_sBuffer.c_str();
     }
-    void Reset(const std::string& sPath)
+    void Reset(const std::string& sTarget, bool bIsFilePath = true)
     {
-        if (m_File.is_open())
+        m_bFileStream = bIsFilePath;
+        if (isFileStream())
         {
-            m_File.close();
-            m_File.open(sPath);
+            if (m_File.is_open())
+            {
+                m_File.close();
+                m_File.open(sTarget);
+            }
+        }
+        else
+        {
+            m_sStream.clear();
+            m_sStream = std::move(std::istringstream(sTarget));
         }
         m_sBuffer.clear();
         m_sResidue.clear();
     }
+    bool isFileStream() const { return (m_bFileStream != false); }
+    CPacker& addStringProcessing(string_processing&& p)
+    {
+        m_vProcess.push_back(std::move(p));
+        return *this;
+    }
+
 private:
     std::size_t m_packageSize;
     std::string m_sBuffer;
     std::string m_sResidue;
     std::ifstream m_File;
+    std::istringstream m_sStream;
+    bool m_bFileStream;
+    std::vector<string_processing> m_vProcess;
 };
 
 int main()
 {
     const char* pack;
-    CAudiotunePackage f(73, "test.txt");
+    CPacker f("test.txt",true);
+    
+    f.addStringProcessing(CPacker::trimmer())
+     .addStringProcessing(CPacker::extra_space_remover())
+     .addStringProcessing(CPacker::space_masking())
+     .addStringProcessing(CPacker::append_new_line());
+    
     while ((pack = f.getPackage()) != NULL)
     {
         std::cout << "------- package --------\n" 
