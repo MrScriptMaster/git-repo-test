c5e07703821cdcf2a7988b973bdc5b1735c18f69
Grigorii <ohmak88@ya.ru>
2020-03-23 14:13:46 +0300

    Update useful_macro_defs.cpp

--- idioms/useful_macro_defs.cpp
+++ idioms/useful_macro_defs.cpp
@@ -69,6 +69,31 @@ public:\
 #define __VA_ARGS__ -1//__macro_make_string("__VA_ARGS__ is not supported")
 #endif
 
+#ifndef delimiter 
+    #define delimiter , 
+#endif 
+ 
+// Макросы для формирования перечислений 
+#ifndef enumeration_begin 
+    #define enumeration_begin(arg) enum arg { 
+#endif 
+ 
+#ifndef enumeration_end 
+    #ifdef last_enumerator 
+        #define enumeration_end delimiter last_enumerator } 
+    #else 
+        #define enumeration_end } 
+    #endif 
+#endif 
+ 
+#ifndef declare_member 
+    #define declare_member(arg) arg 
+#endif 
+ 
+#ifndef member_value 
+    #define member_value(arg) = arg 
+#endif 
+
 #define NDEBUG
 
 #include <cassert>
